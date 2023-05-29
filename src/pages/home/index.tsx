575a344a55e0a68e7cc9bac12edd033498af09c4
Felipe Ribeiro <piporibeiro@gmail.com>
2023-05-29 16:50:45 -0300

    feat: create home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,30 @@
+import { Container, Hero, Preview } from '@/src/pages/home/styles'
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+
+import previewImage from '../../assets/app_preview.png'
+import { ClaimUserNameForm } from '@/src/pages/home/components/ClaimUserNameForm'
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size={'4xl'}>Agendamento descomplicado</Heading>
+        <Text size={'xl'}>
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+        <ClaimUserNameForm />
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          alt={'Calendário símbolizando a aplicação em funcionamento'}
+          quality={100}
+          priority
+          width={827}
+          height={442}
+        />
+      </Preview>
+    </Container>
+  )
+}
