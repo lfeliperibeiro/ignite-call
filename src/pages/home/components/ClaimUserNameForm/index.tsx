c585b12d68ea00551a54cae7d72fd208e5639ad6
Felipe Ribeiro <piporibeiro@gmail.com>
2023-05-30 16:50:31 -0300

    feat: submitting for register route

--- src/pages/home/components/ClaimUserNameForm/index.tsx
+++ src/pages/home/components/ClaimUserNameForm/index.tsx
@@ -4,6 +4,7 @@ import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/router'
 
 const claimUsernameFormSchema = z.object({
   username: z
@@ -21,13 +22,16 @@ export function ClaimUserNameForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<ClaimUserNameFormData>({
     resolver: zodResolver(claimUsernameFormSchema),
   })
 
+  const router = useRouter()
   async function handleClaimUsernameForm(data: ClaimUserNameFormData) {
-    console.log(data)
+    const { username } = data
+
+    await router.push(`/register?username=${username}`)
   }
   return (
     <>
@@ -38,7 +42,7 @@ export function ClaimUserNameForm() {
           placeholder={'seu-usuario'}
           {...register('username')}
         />
-        <Button size={'sm'} type={'submit'}>
+        <Button size={'sm'} type={'submit'} disabled={isSubmitting}>
           Reservar
           <ArrowRight />
         </Button>
