f542824d0b194efc985c582fcc440cdfb2e79add
Felipe Ribeiro <piporibeiro@gmail.com>
2023-06-20 10:49:00 -0300

    refactor: extend more type for request and response

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -1,11 +1,11 @@
 import NextAuth, { NextAuthOptions } from 'next-auth'
-import { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse, NextPageContext } from 'next'
 import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google'
 import { PrismaAdapter } from '@/src/lib/auth/prisma-adapter'
 
 export function buildNextAuthOptions(
-  req: NextApiRequest,
-  res: NextApiResponse,
+  req: NextApiRequest | NextPageContext['req'],
+  res: NextApiResponse | NextPageContext['res'],
 ): NextAuthOptions {
   return {
     adapter: PrismaAdapter(req, res),
