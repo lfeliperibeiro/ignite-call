d8deb7a3171f9f8b99f8adc45ed11d847e666710
Felipe Ribeiro <piporibeiro@gmail.com>
2023-06-22 15:36:00 -0300

    feat: create schedule

--- src/pages/schedule/[username]/index.page.tsx
+++ src/pages/schedule/[username]/index.page.tsx
@@ -2,6 +2,7 @@ import { Container, UserHeader } from './styles'
 import { Avatar, Heading, Text } from '@ignite-ui/react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { prisma } from '@/src/lib/prisma'
+import { ScheduleForm } from './ScheduleForm/index.page'
 
 interface ScheduleProps {
   user: {
@@ -19,6 +20,7 @@ export default function Schedule({ user }: ScheduleProps) {
         <Heading>{user.name}</Heading>
         <Text>{user.bio}</Text>
       </UserHeader>
+      <ScheduleForm />
     </Container>
   )
 }
