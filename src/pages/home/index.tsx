0af9b3b9c58486a1a6c70c78fe65cafbfd9bdb60
Felipe Ribeiro <piporibeiro@gmail.com>
2023-07-10 11:27:26 -0300

    feat: add seo

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,27 +4,34 @@ import Image from 'next/image'
 
 import previewImage from '../../assets/app_preview.png'
 import { ClaimUserNameForm } from '@/src/pages/home/components/ClaimUserNameForm'
+import { NextSeo } from 'next-seo'
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading size={'4xl'}>Agendamento descomplicado</Heading>
-        <Text size={'xl'}>
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.
-        </Text>
-        <ClaimUserNameForm />
-      </Hero>
-      <Preview>
-        <Image
-          src={previewImage}
-          alt={'Calendário símbolizando a aplicação em funcionamento'}
-          quality={100}
-          priority
-          width={827}
-          height={442}
-        />
-      </Preview>
-    </Container>
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre."
+      />
+      <Container>
+        <Hero>
+          <Heading size={'4xl'}>Agendamento descomplicado</Heading>
+          <Text size={'xl'}>
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo livre.
+          </Text>
+          <ClaimUserNameForm />
+        </Hero>
+        <Preview>
+          <Image
+            src={previewImage}
+            alt={'Calendário símbolizando a aplicação em funcionamento'}
+            quality={100}
+            priority
+            width={827}
+            height={442}
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
--- src/pages/schedule/[username]/index.page.tsx
+++ src/pages/schedule/[username]/index.page.tsx
@@ -3,6 +3,7 @@ import { Avatar, Heading, Text } from '@ignite-ui/react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { prisma } from '@/src/lib/prisma'
 import { ScheduleForm } from './ScheduleForm/index.page'
+import { NextSeo } from 'next-seo'
 
 interface ScheduleProps {
   user: {
@@ -14,14 +15,17 @@ interface ScheduleProps {
 
 export default function Schedule({ user }: ScheduleProps) {
   return (
-    <Container>
-      <UserHeader>
-        <Avatar src={user.avatarUrl} />
-        <Heading>{user.name}</Heading>
-        <Text>{user.bio}</Text>
-      </UserHeader>
-      <ScheduleForm />
-    </Container>
+    <>
+      <NextSeo title={`Agendar com ${user.name} | Ignite Call`} />
+      <Container>
+        <UserHeader>
+          <Avatar src={user.avatarUrl} />
+          <Heading>{user.name}</Heading>
+          <Text>{user.bio}</Text>
+        </UserHeader>
+        <ScheduleForm />
+      </Container>
+    </>
   )
 }
 
